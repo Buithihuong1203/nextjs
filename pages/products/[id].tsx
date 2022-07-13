d0e2d6211d554338b0de0459a414de70824a2d55
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-07-13 22:00:31 +0700

    update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from 'next'
+import { GetServerSideProps, GetServerSidePropsContext, GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+
 type ProductProps = {
   product: any
 }
@@ -7,41 +9,52 @@ type ProductProps = {
 const ProductDetail = ({product}: ProductProps) => {
   if(!product) return null;
   return (
-    <div>ProductDetail {product.name}</div>
+    <div>ProductDetail : {product.name}</div>
   )
 }
 
 
-export const getStaticPaths : GetStaticPaths = async () => {
-  const data = await (await fetch(`http://localhost:3002/products`)).json();
-  const paths = data.map(item => {
-    return {params : {id: item.id}}
-  })
-  return {
-    paths,
-    fallback: false
-  }
-}
-export const getStaticProps : GetStaticProps<ProductProps> = async (
-  context : GetStaticPropsContext
-) => {
-  console.log('GET statics props');
-  console.log('context', context.params?.id);
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const data = await (await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products`)).json();
+//   console.log('data', data);
+//   const paths = data.map(item => {
+//     return { params: { id: item.id} }
+//   })
+//   return {
+//     paths,
+//     fallback: 'blocking'
+//   }
+// }
 
-  //call api
-  const data = await (await fetch(`http://localhost:3002/products/${context.params?.id}`)).json()
-  if(!data) {
-    return {
-      notFound: true
-    }
-  }
-  return {
-    props: {
-      product: data
-    }
-  }
-}
+// export const getStaticProps: GetStaticProps<ProductProps> = async (
+//   context: GetStaticPropsContext
+// ) => {
+//   const data = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params.id}`)).json()
+//   if(!data){
+//     return {
+//       notFound: true
+//     }
+//   }
+
+//   return {
+//       props: {
+//           product: data,
+//       },
+//       revalidate: 10
+//   };
+// };
+
+// export const getServerSideProps: GetServerSideProps = async ( context: GetServerSidePropsContext) => {
+//   context.res.setHeader(
+//     'Cache-Control',
+//     's-maxage=5, stale-while-revalidate'
+//   )
+//   const data = await ( await fetch(`https://6110f09bc38a0900171f0ed0.mockapi.io/products/${context.params.id}`)).json()
+//   return {
+//     props: {
+//       product: data
+//     }
+//   }
+// }
 
 export default ProductDetail
-
-
