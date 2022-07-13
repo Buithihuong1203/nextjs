73906cbd01abc699438512130c321e9e6c41ffb5
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-07-13 21:55:52 +0700

    com

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout';
+import LayoutAdmin from '../components/Layout/admin';
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <div>About</div>
+  )
+}
+About.Layout = LayoutAdmin
+export default About
---
+++ pages/products/[id].tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from 'next'
+type ProductProps = {
+  product: any
+}
+
+const ProductDetail = ({product}: ProductProps) => {
+  if(!product) return null;
+  return (
+    <div>ProductDetail {product.name}</div>
+  )
+}
+
+
+export const getStaticPaths : GetStaticPaths = async () => {
+  const data = await (await fetch(`http://localhost:3002/products`)).json();
+  const paths = data.map(item => {
+    return {params : {id: item.id}}
+  })
+  return {
+    paths,
+    fallback: false
+  }
+}
+export const getStaticProps : GetStaticProps<ProductProps> = async (
+  context : GetStaticPropsContext
+) => {
+  console.log('GET statics props');
+  console.log('context', context.params?.id);
+
+  //call api
+  const data = await (await fetch(`http://localhost:3002/products/${context.params?.id}`)).json()
+  if(!data) {
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props: {
+      product: data
+    }
+  }
+}
+
+export default ProductDetail
+
+
