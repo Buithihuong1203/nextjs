91fe1516382c69008c3722471eca0c39c5730656
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-07-13 21:55:33 +0700

    updateeee

---
+++ pages/products/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {GetStaticProps, GetStaticPropsContext } from "next";
+import Link from 'next/link';
+type ProductProps = {
+    products: any[];
+}
+const url = 'https://6110f09bc38a0900171f0ed0.mockapi.io/products';
+
+const fetcher = async (url) => await (await fetch(url)).json()
+//client
+
+// const ProductPage = ({products}: ProductProps) => {
+//     //console.log("Product Page");
+//     //console.log("products", products);
+//     if(!products) return null;
+//     return (
+//         <div>
+//             {products.map((item) => (
+//                 <div key={item.id}><Link href={`/products/${item.id}`}>{item.name}</Link></div>
+//             ))}
+//         </div>
+//     )
+// }
+
+
+// //chạy ở server 
+// //chạy đầu tiên 
+// export const getStaticProps: GetStaticProps<ProductProps> = async ( 
+//     context : GetStaticPropsContext
+// ) => {
+//     const data = await (await fetch(`http://localhost:3002/products`)).json();
+//     console.log('data', data);
+//     if(!data) {
+//         return {
+//             notFound: true
+//         }
+//     }
+//     return {
+//         props: {
+//             products: data
+//         }
+//     }
+// }
+// export default ProductPage
+
+
+// //  getStaticProps: 
